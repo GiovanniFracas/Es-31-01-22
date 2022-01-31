e9ef23279ca209c93a8f4bf8ee96838d17cc514b
GiovanniFracas <98754833+GiovanniFracas@users.noreply.github.com>
2022-01-31 17:55:37 +0100

    Add files via upload

---
+++ script.ts
@@ -0,0 +1,58 @@
+let valore1: number;
+let valore2: number;
+let result: number;
+let result2: number;
+let risultato: number;
+let numeroRand: number;
+let confronto: number;
+let confronto2: number;
+
+$(() => {
+    numeroRand = Math.random() * 100;
+    $('#Restart').hide();
+    $('#verifica').on('click', function () {
+        $('#Restart').show();
+        $('#inizio').html('il numero da indovinare era: ' + numeroRand.toFixed(0));
+        valore1 = Number($('#p1').val());
+        valore2 = Number($('#p2').val());
+        if (valore1 != 0 && valore2 != 0) {
+
+            result = valore1;
+            result2 = valore2;
+            if (valore1 < 0) {
+                result = valore1 * -1;
+            }
+            if (valore2 < 0) {
+                result2 = valore2 * -1;
+            }
+        }
+        confronto = result - numeroRand;
+        confronto2 = result2 - numeroRand;
+        console.log('primo confronto ' + confronto);
+        console.log('primo confronto ' + confronto2);
+
+        if (confronto < 0) {
+            confronto *= -1;
+        }
+        if (confronto2 < 0) {
+            confronto2 *= -1;
+        }
+
+        if (confronto == numeroRand) {
+            $('#finale').html('ha vinto il giocatore 1');
+        }
+        if (confronto2 == numeroRand) {
+            $('#finale').html('ha vinto il giocatore 2');
+        }
+
+        if (confronto < confronto2 && confronto2 != numeroRand && confronto != numeroRand) {
+            console.log('nessuno ha indovinato ma ha vinto il giocatore 1');
+            $('#finale').html('nessuno ha indovinato ma il giocatore 1 si è avvicinato di più');
+        }
+        if (confronto > confronto2 && confronto2 != numeroRand && confronto != numeroRand) {
+            console.log('nessuno ha indovinato ma ha vinto il giocatore 2');
+            $('#finale').html('nessuno ha indovinato ma il giocatore 2 si è avvicinato di più');
+        }
+        console.log(numeroRand);
+    })
+})  
